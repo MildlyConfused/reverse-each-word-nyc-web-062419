6ac7086afeec0dcc057239e341170d8aa61f9c25
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-16 15:07:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,8 @@ def reverse_each_word(sentence)
   words = sentence.split
   rwords = []
   words.collect do |word|
-    id = words.index word
-    rword = word.reverse 
-    rwords[id] = rword
+    id = words.index word 
+    rwords[id] = word.reverse
   end
 
 end
