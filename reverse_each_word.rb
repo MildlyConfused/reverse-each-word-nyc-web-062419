d7fbcb618f548edc74fd94d8217c017842a13506
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-16 15:15:49 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_word(sentence)
+  words = sentence.split
+  rwords = []
+  s = ""
+  words.collect do |word|
+    id = words.index word 
+    rwords[id] = word.reverse
+    if id < words.size-1
+      s = s + word.reverse + " "
+    else 
+      s = s + word.reverse
+    end
+  end
+  puts s
+  return s
+end
+
+
+reverse_each_word("Hello, there and how are you?")
